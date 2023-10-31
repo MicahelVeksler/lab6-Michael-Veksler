7847e57654ed306bb4fbf9df3645fe43a4b225d7
nfield05 <nathanifield556@gmail.com>
2023-10-31 17:27:24 -0400

    Nathan Field's part

--- password.py
+++ password.py
@@ -15,12 +15,27 @@ def menu():
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
+#This is done by Nathan Field
+def decode(pword):
+    decoded=""
+    for i in pword:
+        decoded_dig = str((int(i) - 3) % 10)
+        decoded += decoded_dig
+    return decoded
+
 
 while True:
     menu()
     choice = int(input("Please enter an option:"))
 
     if choice == 1:
-        password = input("Please enter your password to encode:")
+        password = encode_password(input("Please enter your password to encode:"))
         print("Your password has been encoded and stored!")
+    if choice == 2:
+
+        print(f"The encoded password is {password}, and the original password is {decode(password)}")
+    if choice == 3:
+        break
+
+
 
